c9522625993e52cf347b75a8a433a125e93d732e
Elkin Kirill <kirillv.2016@mail.ru>
2025-04-19 05:02:54 +0300

    need

--- web_service/main/decorators.py
+++ web_service/main/decorators.py
@@ -1,19 +1,13 @@
-from django.shortcuts import redirect
-from django.http import HttpResponse
-
-
 def role_required(role_name):
     def decorator(view_func):
         def _wrapped_view(request, *args, **kwargs):
             if 'user_id' not in request.session:
                 return redirect('/login/')
 
-            # user_id = request.session['user_id']
-            user = {
-                "role": "moderator"
-            }
+            user_id = request.session['user_id']
+            user = get_user_from_your_db(user_id)  # Ваша функция для получения пользователя
 
-            if user["role"] != role_name:
+            if user.role != role_name:  # Проверяем роль
                 return HttpResponse("Доступ запрещен", status=403)
 
             return view_func(request, *args, **kwargs)

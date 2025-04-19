e6b0bf27dc48a5fcee1bc27f99616f559c451acb
der-johanni <ivangorbanwork@gmail.com>
2025-04-20 02:55:31 +0300

    users handlers added

--- enternal/handlers/users.py
+++ enternal/handlers/users.py
@@ -15,3 +15,21 @@ async def get_user_info(user: User):
 @user_router.get('/users')
 async def get_users_info():
     pass
+
+
+@user_router.create('/user/create')
+async def create_user(user: User):
+    return {
+        'id': user.id,
+        'login': user.login,
+        'password_hashed': user.password_hash
+    }
+
+
+@user_router.delete('/user/delete')
+async def delete_user(user: User):
+    return {
+        'id': user.id,
+        'login': user.login,
+        'password_hashed': user.password_hash
+    }

03d12e42f908a0f55bab7a9bf2656087019d2268
Trum <artamarkan@gmail.com>
2025-04-19 20:08:05 +0300

    stage commit

--- storage/app/models.py
+++ storage/app/models.py
@@ -21,12 +21,10 @@ class Publication(Base):
     created_at: Mapped[datetime] = mapped_column(DateTime)
 
     # Foreign keys
-    author_id: Mapped[str] = mapped_column(String, ForeignKey("Authors.id"))
-    company_id: Mapped[str] = mapped_column(String, ForeignKey("Companies.id"))
+    author_id: Mapped[str] = mapped_column(String, ForeignKey("Users.id"))
 
     # Relationships
-    author: Mapped["Author"] = relationship(back_populates="publications")
-    company: Mapped["Company"] = relationship(back_populates="publications")
+    author: Mapped["User"] = relationship(back_populates="publications")
 
 
 class Company(Base):
@@ -37,64 +35,25 @@ class Company(Base):
     image_url: Mapped[str] = mapped_column(String)
 
     # Relationships
-    publications: Mapped[list["Publication"]] = relationship(
-        back_populates="company"
-    )
-    authors: Mapped[list["Author"]] = relationship(back_populates="company")
+    authors: Mapped[list["User"]] = relationship(back_populates="company")
     subscriptions: Mapped[list["Subscription"]] = relationship(
         back_populates="company"
     )
 
-    def to_json(self, include_relationships: bool = True) -> dict[str, Any]:
-        result = {
-            "id": self.id,
-            "name": self.name,
-            "description": self.description,
-            "image_url": self.image_url,
-        }
 
-        if include_relationships:
-            result.update(
-                {
-                    "publications": [
-                        p.to_json(include_relationships=False)
-                        for p in self.publications
-                    ],
-                    "authors": [
-                        a.to_json(include_relationships=False)
-                        for a in self.authors
-                    ],
-                    "subscriptions": [
-                        s.to_json(include_relationships=False)
-                        for s in self.subscriptions
-                    ],
-                }
-            )
-        return
-
-
-class UserBase:
+class User(Base):
+    __tablename__ = "Users"
     id: Mapped[str] = mapped_column(String, primary_key=True)
-    first_name: Mapped[str] = mapped_column(String)
-    last_name: Mapped[str] = mapped_column(String)
     login: Mapped[str] = mapped_column(String)
     password: Mapped[str] = mapped_column(String)
     role: Mapped[str] = mapped_column(String)
-
-
-class Author(UserBase, Base):
-    __tablename__ = "Authors"
-    company_id: Mapped[str] = mapped_column(String, ForeignKey("Companies.id"))
+    company_id: Mapped[str] = mapped_column(
+        String, ForeignKey("Companies.id"), nullable=True
+    )
 
     # Relationships
     company: Mapped["Company"] = relationship(back_populates="authors")
-    publications: Mapped[list["Publication"]] = relationship(
-        back_populates="author"
-    )
-
-
-class Moderator(UserBase, Base):
-    __tablename__ = "Moderators"
+    publications: Mapped["Publication"] = relationship(back_populates="author")
 
 
 class Subscriber(Base):
@@ -111,7 +70,7 @@ class Subscriber(Base):
 class Subscription(Base):
     __tablename__ = "Subscriptions"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    subscriber_id: Mapped[int] = mapped_column(
+    subscriber_id: Mapped[str] = mapped_column(
         Integer, ForeignKey("Subscribers.id")
     )
     company_id: Mapped[str] = mapped_column(String, ForeignKey("Companies.id"))
--- storage/app/schemas.py
+++ storage/app/schemas.py
@@ -1,7 +1,10 @@
-from marshmallow import Schema, fields, validate
+from marshmallow import EXCLUDE, Schema, fields, validate
 
 
 class PublicationSchema(Schema):
+    class Meta:
+        unknown = EXCLUDE
+
     id = fields.Str(required=True)
     title = fields.Str(required=True)
     content = fields.Str(required=True)
@@ -10,57 +13,52 @@ class PublicationSchema(Schema):
 
     # Foreign keys
     author_id = fields.Str(required=True)
-    company_id = fields.Str(required=True)
 
-    # Relationships (nested schemas)
-    author = fields.Nested("AuthorSchema", exclude=("publications",))
-    company = fields.Nested("CompanySchema", exclude=("publications",))
+    # Relationships
+    author = fields.Nested("UserSchema", exclude=("publications", "company"))
 
 
 class CompanySchema(Schema):
+    class Meta:
+        unknown = EXCLUDE
+
     id = fields.Str(required=True)
     name = fields.Str(required=True)
     description = fields.Str(required=True)
     image_url = fields.Str(required=True)
 
     # Relationships
-    publications = fields.List(
-        fields.Nested(PublicationSchema, exclude=("company",))
+    authors = fields.List(
+        fields.Nested("UserSchema", exclude=("company", "publications"))
     )
-    authors = fields.List(fields.Nested("AuthorSchema", exclude=("company",)))
     subscriptions = fields.List(
         fields.Nested("SubscriptionSchema", exclude=("company",))
     )
 
 
-class UserBaseSchema(Schema):
+class UserSchema(Schema):
+    class Meta:
+        unknown = EXCLUDE
+
     id = fields.Str(required=True)
-    first_name = fields.Str(required=True)
-    last_name = fields.Str(required=True)
     login = fields.Str(required=True)
-    password = fields.Str(
-        required=True, load_only=True
-    )  # Пароль не должен возвращаться в ответах
+    password = fields.Str(required=True, load_only=True)
     role = fields.Str(
-        required=True, validate=validate.OneOf(["author", "moderator"])
+        required=True, validate=validate.OneOf(["author", "moderator", "admin"])
     )
-
-
-class AuthorSchema(UserBaseSchema):
-    company_id = fields.Str(required=True)
+    company_id = fields.Str(allow_none=True)
 
     # Relationships
-    company = fields.Nested(CompanySchema, exclude=("authors",))
+    company = fields.Nested(CompanySchema, exclude=("authors", "subscriptions"))
     publications = fields.List(
         fields.Nested(PublicationSchema, exclude=("author",))
     )
 
 
-class ModeratorSchema(UserBaseSchema):
-    pass
-
-
 class SubscriberSchema(Schema):
+    class Meta:
+        unknown = EXCLUDE
+
     id = fields.Int(required=True)
     telegram_id = fields.Str(required=True)
 
@@ -71,10 +69,43 @@ class SubscriberSchema(Schema):
 
 
 class SubscriptionSchema(Schema):
+    class Meta:
+        unknown = EXCLUDE
+
     id = fields.Int(required=True)
     subscriber_id = fields.Int(required=True)
     company_id = fields.Str(required=True)
 
     # Relationships
     subscriber = fields.Nested(SubscriberSchema, exclude=("subscriptions",))
-    company = fields.Nested(CompanySchema, exclude=("subscriptions",))
+    company = fields.Nested(CompanySchema, exclude=("subscriptions", "authors"))
+
+
+class PublicationCreateSchema(Schema):
+    class Meta:
+        unknown = EXCLUDE
+
+    title = fields.Str(required=True, validate=validate.Length(min=1, max=255))
+    content = fields.Str(required=True)
+    main_image_url = fields.Str(required=True, validate=validate.URL())
+    author_id = fields.Str(required=True)
+
+
+class UserCreateSchema(Schema):
+    class Meta:
+        unknown = EXCLUDE
+
+    login = fields.Str(required=True, validate=validate.Length(min=3, max=50))
+    password = fields.Str(required=True, validate=validate.Length(min=6))
+    role = fields.Str(
+        required=True, validate=validate.OneOf(["author", "moderator", "admin"])
+    )
+    company_id = fields.Str(allow_none=True)
+
+
+class SubscriptionCreateSchema(Schema):
+    class Meta:
+        unknown = EXCLUDE
+
+    subscriber_id = fields.Int(required=True)
+    company_id = fields.Str(required=True)

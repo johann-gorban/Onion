530e32c349a7fb27fa707c1ca452211c61436a93
der-johanni <ivangorbanwork@gmail.com>
2025-04-20 06:41:47 +0300

    posts services added

--- enternal/services/posts.py
+++ enternal/services/posts.py
@@ -1,25 +1,34 @@
 from enternal.services.models.domains import Publication, User
 from enternal.storages.sqlalchemy_db import db
 
+
 async def get_post_by_id(post_id: str) -> Publication | None:
     return await db.get_publication(post_id)
 
 
-async def get_posts_by_author(author: "User") -> list["Publication"]:
-    """Получение публикаций конкретного автора"""
+async def get_posts() -> list[Publication]:
+    return await db.get_publications()
 
 
-async def create_post(publication_data: "Publication") -> "Publication":
-    """Создание новой публикации"""
+async def create_post(post: Publication) -> None:
+    return await db.add_publication(
+        title=post.title,
+        content=post.content,
+        image_url=post.main_image_url,
+        author_id=post.author,
+        created_at=post.created_at
+    )
 
 
-async def update_post(post_id: str, update_data: dict) -> "Publication":
-    """Обновление существующей публикации"""
+async def update_post(updated_post: Publication) -> None:
+    return await db.update_publication(
+        title=updated_post.title,
+        content=updated_post.content,
+        image_url=updated_post.main_image_url,
+        author_id=updated_post.author,
+        created_at=updated_post.created_at
+    )
 
 
-async def delete_post(post_id: str) -> bool:
-    """Удаление публикации"""
-
-
-async def search_posts(query: str) -> list["Publication"]:
-    """Поиск публикаций по текстовому запросу"""
+async def delete_post(post_id: str) -> None:
+    await db.remove_publication(post_id)

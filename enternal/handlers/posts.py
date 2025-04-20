4da823e4110fc50512b5606324066b293f0c77ac
der-johanni <ivangorbanwork@gmail.com>
2025-04-20 07:01:19 +0300

    posts service added

--- enternal/handlers/posts.py
+++ enternal/handlers/posts.py
@@ -1,53 +1,29 @@
 from fastapi import APIRouter
 from services.domains import Publication
+from services.posts import get_posts, get_post_by_id, create_post, update_post, delete_post
 
 post_router = APIRouter()
 
 @post_router.get('/posts/')
-async def get_all_posts():
-    pass
+async def handler_get_all_posts():
+    return await get_posts()
 
 
 @post_router.get('/post_by_id/')
-async def get_post_by_id(post: Publication):
-    return {
-        'title': post.title,
-        'content': post.content,
-        'author_id': post.author_id,
-        'image': post.main_image_url,
-        'date': post.created_at
-    }
+async def handler_get_post_by_id(post: Publication):
+    return await get_post_by_id(post.id)
 
 
 @post_router.post('/posts/create')
-async def create_post(post: Publication):
-    return {
-        'title': post.title,
-        'content': post.content,
-        'author_id': post.author_id,
-        'image': post.main_image_url,
-        'date': post.created_at
-    }
+async def handler_create_post(post: Publication):
+    await create_post(post)
 
 
 @post_router.patch('/posts/update')
 async def update_post(post: Publication):
-    return {
-        'title': post.title,
-        'content': post.content,
-        'author_id': post.author_id,
-        'image': post.main_image_url,
-        'date': post.created_at
-    }
+    await update_post(post)
 
 
 @post_router.delete('/posts/delete')
 async def delete_post(post: Publication):
-    return {
-        'id': post.id,
-        'title': post.title,
-        'content': post.content,
-        'author_id': post.author_id,
-        'image': post.main_image_url,
-        'date': post.created_at
-    }
+    await delete_post(post)
